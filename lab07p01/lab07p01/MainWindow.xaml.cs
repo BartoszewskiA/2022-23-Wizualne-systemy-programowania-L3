fb9d0ee83858733ec31c3473d63ebfbdbf4461a0
BartoszewskiA <bartoszewski.a@outlook.com>
2023-01-10 17:47:20 +0100

    10.01.23

---
+++ lab07p01/lab07p01/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Color = System.Windows.Media.Color;
+using FontFamily = System.Windows.Media.FontFamily;
+
+namespace lab07p01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            oknoTekstowe.Text = "  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum convallis, quam nec malesuada rhoncus, odio metus vestibulum quam, in laoreet ex odio scelerisque mi. Sed sit amet egestas augue. Proin fermentum sit amet nibh eget facilisis. Suspendisse ultrices et lacus at placerat..";
+        }
+
+        private void bt_styl_fomtu_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FontDialog fontDialog1 = new System.Windows.Forms.FontDialog();
+            if(fontDialog1.ShowDialog()== System.Windows.Forms.DialogResult.OK)
+            {
+                oknoTekstowe.FontFamily = new FontFamily(fontDialog1.Font.FontFamily.Name.ToString());
+                oknoTekstowe.FontSize = fontDialog1.Font.Size;
+            }
+        }
+
+        private void bt_kolor_fomtu_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.ColorDialog colorDialog1 = new System.Windows.Forms.ColorDialog();
+            if(colorDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                byte R = colorDialog1.Color.R;
+                byte G = colorDialog1.Color.G;
+                byte B = colorDialog1.Color.B;
+                Color kolor = Color.FromRgb(R, G, B);
+                oknoTekstowe.Foreground = new SolidColorBrush(kolor);
+            }
+           
+        }
+
+        private void bt_kolor_tla_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.ColorDialog colorDialog1 = new System.Windows.Forms.ColorDialog();
+            if (colorDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                byte R = colorDialog1.Color.R;
+                byte G = colorDialog1.Color.G;
+                byte B = colorDialog1.Color.B;
+                Color kolor = Color.FromRgb(R, G, B);
+                oknoTekstowe.Background = new SolidColorBrush(kolor);
+            }
+        }
+
+        private void suwak_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            oknoTekstowe.FontSize = suwak.Value;
+        }
+    }
+}
---
+++ lab08p01/lab08p01/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab08p01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFileDialog oknoOtwierania;
+        SaveFileDialog oknoZapisywania;
+        public MainWindow()
+        {
+            InitializeComponent();
+            oknoOtwierania = new OpenFileDialog();
+            oknoOtwierania.Title = "Otwórz plik";
+            oknoOtwierania.Filter = "Pliki tekstowe|*.txt|Wszystkie pliki|*.*";
+            oknoZapisywania = new SaveFileDialog();
+            oknoZapisywania.Title = "Zapisz plik";
+            oknoZapisywania.Filter = "Pliki tekstowe|*.txt|Wszystkie pliki|*.*";
+            oknoZapisywania.DefaultExt = "txt";
+        }
+
+        private void bt_open_Click(object sender, RoutedEventArgs e)
+        {
+            if(oknoOtwierania.ShowDialog()==true)
+            {
+                oknoTekstowe.Text = File.ReadAllText(oknoOtwierania.FileName);
+            }
+
+        }
+
+        private void bt_save_Click(object sender, RoutedEventArgs e)
+        {
+            if (oknoZapisywania.ShowDialog() == true)
+            {
+                File.WriteAllText(oknoZapisywania.FileName, oknoTekstowe.Text);
+            }
+        }
+    }
+}
